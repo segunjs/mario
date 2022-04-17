5566cd459678938595ca51c91ced68c56eb40047
segunjs <segunnfayomi@gmail.com>
2022-04-17 11:31:25 +0100

    project

---
+++ project.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "cs50.h"
+
+int main(void)
+{
+    int numbers[] = {4,6,8,2,7,5,0};
+
+    for(int i = 0; i < 7; i++)
+    {
+        if(numbers[i] == 3)
+        {
+            printf("found\n");
+            return 0;
+        }
+    }
+    printf("not found\n");
+    return 1;
+
+}
